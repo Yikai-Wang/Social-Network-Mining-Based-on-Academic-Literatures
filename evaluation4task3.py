65e82af893345c57f017b7c73144b43ba1480ec1
Wang-Yikai <yikaiwang18@gmail.com>
2018-06-10 21:43:46 +0800

    update evaluation

--- evaluation4task3.py
+++ evaluation4task3.py
@@ -9,43 +9,47 @@ import pickle
 import json
 from argparse import ArgumentParser,ArgumentDefaultsHelpFormatter
 
-def AUC(task,exist_edges,missing_edges,authors,times,node_vector,confs=[]):
+def AUC(task,nonexist,missing_edges,authors,times,node_vector,confs=[]):
     if task == 'cp' or task == 're':
-        nonexist_edges = []
-        tmp = exist_edges + missing_edges
-        for i in range(len(authors)-1):
-            for j in range(i+1, len(authors)):
-                if ([authors[i],authors[j]] not in tmp) and ([authors[j],authors[i]] not in tmp):
-                    nonexist_edges.append([authors[i],authors[j]])
         n1 = 0
         n2 = 0
-        for _ in range(times):
-            missing_edge = random.sample(missing_edges)
-            nonexist_edge = random.sample(nonexist_edges)
+        for i in range(times):
+            missing_edge = random.sample(missing_edges,1)[0]
+            nonexist_edge = []
+            while True:
+                tmp1 = random.randint(0,len(authors)-1)
+                tmp2 = random.randint(0,len(authors)-1)
+                if tmp1 == tmp2 or (not nonexist[tmp1][tmp2]):
+                    continue
+                else:
+                    if nonexist[tmp1][tmp2] and nonexist_edge == []:
+                        nonexist_edge = [authors[tmp1],authors[tmp2]]
+                if nonexist_edge != []:
+                    break
             score_miss = np.sum(np.array(node_vector[missing_edge[0]])*np.array(node_vector[missing_edge[1]]))
             score_none = np.sum(np.array(node_vector[nonexist_edge[0]])*np.array(node_vector[nonexist_edge[1]]))
             if score_miss > score_none:
                 n1 += 1
             elif score_miss == score_none:
                 n2 += 1
+            print(i)
         result = (n1+0.5*n2)/times
         print(result)
         return result
     if task == 'conf':
         return
 
-def Precision(task,exist_edges,missing_edges,authors,L,node_vector,confs=[]):
+def Precision(task,nonexist,authors,L,node_vector,confs=[]):
     if task == 'cp' or task == 're':
         non_observed_edges = []
         for i in range(len(authors)-1):
             for j in range(i+1, len(authors)):
-                if ([authors[i],authors[j]] not in exist_edges) and ([authors[j],authors[i]] not in exist_edges):
-                    score = np.sum(np.array(node_vector[i])*np.array(node_vector[j]))
-                    non_observed_edges.append([[authors[i],authors[j]],score])
+                score = np.sum(np.array(node_vector[i])*np.array(node_vector[j]))
+                non_observed_edges.append([[i,j],score])
         non_observed_edges.sort(key= lambda x: -x[1])
         Lr = 0
         for i in range(L):
-            if non_observed_edges[i][0] in missing_edges or [non_observed_edges[i][0][1],non_observed_edges[i][0][0]] in missing_edges:
+            if not nonexist[non_observed_edges[i][0][0]][non_observed_edges[i][0][1]]:
                 Lr += 1
         result = Lr/L
         print(result)
@@ -87,33 +91,50 @@ parser = ArgumentParser('evaluation',
 parser.add_argument('--task', default=False,
                         help='cp/re/conf')
 args = parser.parse_args()
-if args.task=='cp' or args.task=='re':
+if args.task=='cp':
     vectors = load_pickle('data/task3'+args.task+'.pkl')
-    train_id = load_txt('data/train_id_4task3.txt')
     valid_id = load_txt('data/valid_id_4task3.txt')
     chosen_authors = list(vectors.keys())
-    exist_edges = []
-    missing_edges = []
+    authors2num = dict()
+    for i in range(len(chosen_authors)):
+        authors2num[chosen_authors[i]]=i
+    nonexist = [[True]*len(chosen_authors)]*len(chosen_authors)
+    papers = load_json('data/allpaper.txt')
+    missing_edges= []
+    for i in range(len(papers)):
+        if str(i) in valid_id:
+            authors = papers[i]['authors']
+            new_authors = [au.replace(' ','') for au in authors]
+            for i in range(len(new_authors)-1):
+                if new_authors[i] in chosen_authors:
+                    for j in range(i+1,len(authors)):
+                        if new_authors[j] in chosen_authors:
+                            nonexist[authors2num[new_authors[i]]][authors2num[new_authors[j]]]=False
+                            nonexist[authors2num[new_authors[j]]][authors2num[new_authors[i]]]=False
+                            missing_edges.append([new_authors[i],new_authors[j]])
+    print('Finished Loading!')
+    AUC(task=args.task,nonexist=nonexist,missing_edges=missing_edges,authors=chosen_authors,times=5000,node_vector=vectors)
+    #Precision(task=args.task,nonexist=nonexist,authors=chosen_authors,L=5000,node_vector=vectors)
+elif args.task=='re':
+    vectors = load_pickle('data/task3'+args.task+'.pkl')
+    valid_id = load_txt('data/valid_id_4task3.txt')
+    chosen_authors = list(vectors.keys())
+    authors2num = dict()
+    for i in range(len(chosen_authors)):
+        authors2num[chosen_authors[i]]=i
+    nonexist = [[False]*len(chosen_authors)]*len(chosen_authors)
     papers = load_json('data/allpaper.txt')
     for i in range(len(papers)):
-        if str(i) in train_id:
+        if str(i) in valid_id:
             authors = papers[i]['authors']
             new_authors = [au.replace(' ','') for au in authors]
             for i in range(len(new_authors)-1):
                 if new_authors[i] in chosen_authors:
                     for j in range(i+1,len(authors)):
                         if new_authors[j] in chosen_authors:
-                            exist_edges.append([new_authors[i],new_authors[j]])
-        elif str(i) in valid_id:
-            authors = papers[i]['authors']
-            new_authors = [au.replace(' ','') for au in authors]
-            for i in range(len(new_authors)-1):
-                if new_authors[i] in chosen_authors:
-                    for j in range(i+1,len(authors)):
-                        if new_authors[j] in chosen_authors:
-                            missing_edges.append([new_authors[i],new_authors[j]])
-    AUC(task=args.task,exist_edges=exist_edges,missing_edges=missing_edges,authors=chosen_authors,times=5000,node_vector=vectors)
-    Precision(task=args.task,exist_edges=exist_edges,missing_edges=missing_edges,authors=chosen_authors,L=5000,node_vector=vectors)
+                            nonexist[authors2num[new_authors[i]]][authors2num[new_authors[j]]]=False
+                            nonexist[authors2num[new_authors[j]]][authors2num[new_authors[i]]]=False
+    AUC(task=args.task,nonexist=nonexist,authors=chosen_authors,times=5000,node_vector=vectors)
 if args.task =='conf':
     vectors = load_pickle('data/task3' + args.task + '.pkl')
     train_id = load_txt('data/train_id_4task3.txt')
@@ -142,8 +163,8 @@ if args.task =='conf':
                             missing_edges.append([new_authors[i], new_authors[j]])
     AUC(task=args.task, exist_edges=exist_edges, missing_edges=missing_edges, authors=chosen_authors, times=5000,
         node_vector=vectors)
-    Precision(task=args.task, exist_edges=exist_edges, missing_edges=missing_edges, authors=chosen_authors, L=5000,
-              node_vector=vectors)
+    #Precision(task=args.task, exist_edges=exist_edges, missing_edges=missing_edges, authors=chosen_authors, L=5000,
+              #node_vector=vectors)
 
 
 
