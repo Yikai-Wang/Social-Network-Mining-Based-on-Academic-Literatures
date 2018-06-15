52d2dc09245f6e64e24ec712b50d425523bf441e
Wang-Yikai <yikaiwang18@gmail.com>
2018-06-15 09:52:10 +0800

    add comments

--- Naive_Clustering_Algorithm.py
+++ Naive_Clustering_Algorithm.py
@@ -10,6 +10,10 @@ import pickle
 from argparse import ArgumentParser,ArgumentDefaultsHelpFormatter
 
 def load_pickle():
+    """
+    load wordvec.pkl
+    :return: wordvec as a dictionary.
+    """
     pkl_file = open('wordvec.pkl', 'rb')
     data1 = pickle.load(pkl_file)
     pkl_file.close()
@@ -17,12 +21,23 @@ def load_pickle():
 
 
 def write_in_pickle(data,name):
+    """
+    :param data: data we need to save
+    :param name: the filename
+    :return: nothing
+    """
     output = open('cluster_'+name+'_nrl.pkl', 'wb')
     pickle.dump(data, output)
     output.close()
 
 
 def list2dict(result,labels,name):
+    """
+    :param result: result of clustering
+    :param labels: the label set in the clustering
+    :param name: filename
+    :return: nothing
+    """
     for_wooden = {}
     for i in result:
         for_wooden[i[0]]=i[1]
@@ -38,6 +53,10 @@ def list2dict(result,labels,name):
 class Clustering():
 
     def __init__(self,data,n_clusters):
+        """
+        :param data: vectors of data and its number.
+        :param n_clusters: number of cluster set.
+        """
         self.n_clusters = n_clusters
         self.id = []
         self.features = []
@@ -46,30 +65,51 @@ class Clustering():
             self.features.append(i[1])
 
     def feature_to_result(self, feature):
+        """
+        save the cluster result.
+        :param feature: the labels we get.
+        :return: each id with its cluster result.
+        """
         result = []
         for i in range(len(self.id)):
             result.append([self.id[i],feature[i]])
         return result
 
     def method_KMeans(self):
+        """
+        Using KMeans to cluster
+        :return: id with label and all label.
+        """
         clf = KMeans(n_clusters=self.n_clusters,max_iter=1000000)
         result = clf.fit(self.features)
         print('Finish result')
         return [self.feature_to_result(result.labels_), list(range(self.n_clusters))]
 
     def method_DBSCAN(self):
+        """
+        Using DBSCAN to cluster
+        :return: id with label and all label.
+        """
         clf = DBSCAN(min_samples=5000,eps=1,n_jobs=-1)
         result = clf.fit(self.features)
         print('Finish result')
         return self.feature_to_result(result.labels_)
 
     def method_MeanShift(self):
+        """
+        Using MeanShift to cluster
+        :return: id with label and all label.
+        """
         clf = MeanShift(n_jobs=-1)
         result = clf.fit(self.features)
         print('Finish result')
         return [self.feature_to_result(result.labels_), list(set(result.labels_))]
 
     def method_Birch(self):
+        """
+        Using Birch to cluster
+        :return: id with label and all label.
+        """
         clf = Birch(n_clusters=self.n_clusters)
         result = clf.fit(self.features)
         print('Finish result')
